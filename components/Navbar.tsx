7779d45b0c04cfefe11177be65a60c7b90d72022
rahul@clearcut.design <rahul@clearcut.design>
2024-04-11 06:02:03 +0700

    mobile-nav which needed to be fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import MobileNav from "./MobileNav";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-centre gap-1">
+        <Image
+          src="/icons/logo.svg"
+          width={32}
+          height={32}
+          alt="Yoom logo"
+          className="max-sm:size-10"
+        ></Image>
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Yoom
+        </p>
+      </Link>
+      <div className="flex-between gap-5">{/* Clerk - User Management */}</div>
+      <MobileNav />
+    </nav>
+  );
 };
