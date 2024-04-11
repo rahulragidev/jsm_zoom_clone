89d2e1f2d77fcb6a96f89c367568c4c0a69347a9
rahul@clearcut.design <rahul@clearcut.design>
2024-04-12 04:10:06 +0700

    date issue fixed

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -18,9 +18,7 @@ const Home = () => {
           </h2>
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">11:30 PM</h1>
-            <p className="text-lg font-medium text-sky-1 lg:text-2xl">
-              Saturday, March 23, 2024
-            </p>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
           </div>
         </div>
       </div>
