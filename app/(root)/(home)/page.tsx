21b44ce32f7d4ceb354387442c5af68dd8b1fce6
Rahul Ragi <iamrahulragi@gmail.com>
2024-05-09 06:58:52 +0530

    minor home page design changes

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,7 +2,6 @@ import MeetingTypeList from "@/components/MeetingTypeList";
 
 const Home = () => {
 	const now = new Date();
-
 	const time = now.toLocaleTimeString("en-US", {
 		hour: "2-digit",
 		minute: "2-digit",
@@ -12,19 +11,19 @@ const Home = () => {
 	);
 
 	return (
-		<section className="flex size-full flex-col gap-5 text-white">
+		<section className="flex flex-col gap-5 text-white min-h-screen">
 			<div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
-				<div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-					{/* <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
-						Upcoming Meeting at: {time}
-					</h2> */}
+				<div className="flex h-full flex-col justify-between p-11 max-md:px-5 max-md:py-8">
 					<div className="flex flex-col gap-2">
-						<h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
-						<p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+						<h1 className="text-7xl font-extrabold lg:text-7xl max-md:text-4xl">
+							{time}
+						</h1>
+						<p className="text-2xl font-medium text-sky-1 lg:text-2xl max-md:text-lg">
+							{date}
+						</p>
 					</div>
 				</div>
 			</div>
-
 			<MeetingTypeList />
 		</section>
 	);
