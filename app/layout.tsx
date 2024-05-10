f8789d1c53b5b0971354aa91cb085d723344bfcc
Rahul Ragi <iamrahulragi@gmail.com>
2024-05-10 18:49:16 +0530

    QuickLink Name Change

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,45 +3,45 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
-import '@stream-io/video-react-sdk/dist/css/styles.css';
+import "@stream-io/video-react-sdk/dist/css/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "YOOM",
-  description: "Video calling app",
-  icons:{
-    icon:'/icons/logo.svg',
-  }
+	title: "QuickLink",
+	description: "Video calling app",
+	icons: {
+		icon: "/icons/logo.svg",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <ClerkProvider
-        appearance={{
-          layout: {
-            logoImageUrl: "/icons/yoom-logo.svg",
-            socialButtonsVariant: "iconButton",
-          },
-          variables: {
-            colorText: "#fff",
-            colorPrimary: "#0E78F9",
-            colorBackground: "#1c1f2e",
-            colorInputBackground: "#252a41",
-            colorInputText: "#fff",
-          },
-        }}
-      >
-        <body className={`${inter.className} bg-dark-2`}>
-          {children}
-          <Toaster />
-        </body>
-      </ClerkProvider>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<ClerkProvider
+				appearance={{
+					layout: {
+						logoImageUrl: "/icons/yoom-logo.svg",
+						socialButtonsVariant: "iconButton",
+					},
+					variables: {
+						colorText: "#fff",
+						colorPrimary: "#0E78F9",
+						colorBackground: "#1c1f2e",
+						colorInputBackground: "#252a41",
+						colorInputText: "#fff",
+					},
+				}}
+			>
+				<body className={`${inter.className} bg-dark-2`}>
+					{children}
+					<Toaster />
+				</body>
+			</ClerkProvider>
+		</html>
+	);
 }
